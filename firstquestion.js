6ccc11bd469514891a046dd7f6e8badc730ac504
PROMIT PAUL <paul.promit.42904@gmail.com>
2026-05-16 22:52:10 +0600

    first question

---
+++ firstquestion.js
@@ -0,0 +1,35 @@
+function validateIdentity(CIN)
+{
+    let isValid = true;
+    
+    if(CIN>1)
+    {
+        for (let i = 2; i < CIN; i++)
+        {
+            if (CIN % i === 0)
+            {
+                isValid = false;
+                break;
+                //return "Rejected: Identity pattern shows external links.";
+            }
+        }
+    }
+    else
+    {
+        isValid = false;
+    }
+    
+    if(isValid)
+    {
+        return "Agent Approved: Identity pattern is untraceable.";
+    }
+    else
+    {
+        return "Rejected: Identity pattern shows external links.";
+    }
+    
+}
+
+const CIN = 91;
+console.log(validateIdentity(CIN));
+
